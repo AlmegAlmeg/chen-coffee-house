bdd35a9cf4aa264194bdbccf5016471d45be09b1
Chen Rozilyo <chen.rozilyo@gmail.com>
2022-12-05 22:36:22 +0200

    Adding HomePage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,17 @@
-import './App.scss';
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import "./App.scss";
 
 function App(): JSX.Element {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </main>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+import "./HomePage.scss";
+
+export default function HomePage() {
+  return <h1>This is My Home Page!</h1>;
+}
