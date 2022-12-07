ded06cf3e0db5cd2ba9bed6d199c8579bf0fbc09
Chen Rozilyo <chen.rozilyo@gmail.com>
2022-12-07 20:55:35 +0200

    Adding NavBar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Navbar from "./pages/components/NavBar";
 import "./App.scss";
 
 function App(): JSX.Element {
   return (
     <>
+      <Navbar />
       <main className="main-content">
         <Routes>
           <Route path="/" element={<HomePage />} />
+          {/* <Route path="/products" element={<Products />} /> */}
         </Routes>
       </main>
     </>
